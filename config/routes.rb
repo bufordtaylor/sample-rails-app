2c202b4ef517eb67825f29812207544426c5ce47
Buford Taylor <bufordtaylor@gmail.com>
2013-03-23 17:12:51 -0700

    Added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
 
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+  root to: 'pages#home'
 
-  get "pages/about"
+  match '/signup', to: 'users#new'
+
+  match '/help', to: 'pages#help'
+  match '/contact', to: 'pages#contact'
+  match '/about', to: 'pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,62 +3,49 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  subject { page }
+
   describe "home page" do
 
+    before { visit root_path }
+
     it "returns http success" do
-      get 'home'
       response.should be_success
     end
 
-    it "should have the right title" do
-      visit '/pages/home'
-      page.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Home") }
 
-    it "should have the h1 'Sample App'" do
-      visit '/pages/home'
-      page.should have_selector('h1', :content => "Sample App")
-    end
+    it { should have_selector('h1', :content => "Sample App") }
 
     it "should have a non blank body" do
-      get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
     end
 
   end
 
   describe "GET 'contact'" do
+
+    before { visit contact_path }
     it "returns http success" do
-      get 'contact'
       response.should be_success
     end
 
-    it "should have the right title" do
-      visit '/pages/contact'
-      page.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Contact") }
 
-    it "should have the h1 'Sample App'" do
-      visit '/pages/contact'
-      page.should have_selector('h1', :content => "Sample App")
-    end
+    it { should have_selector('h1', :content => "Sample App") }
   end
 
   describe "GET 'about'" do
+
+    before { visit about_path }
+
     it "returns http success" do
-      get 'about'
       response.should be_success
     end
 
-    it "should have the right title" do
-      visit '/pages/about'
-      page.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | About")
-    end
+    it { should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | About") }
 
-    it "should have the h1 'Sample App'" do
-      visit '/pages/about'
-      page.should have_selector('h1', :content => "Sample App")
-    end
+    it { should have_selector('h1', :content => "Sample App") }
   end
 
 end
